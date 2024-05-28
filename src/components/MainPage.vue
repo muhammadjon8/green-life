59df4204232142dcdcacdc83fab5795821c1dfbb
muhammadjon8 <umuhammadjon22@gmail.com>
2024-05-29 00:11:33 +0500

    #

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -4,13 +4,21 @@
       <div class="embla__container">
         <div class="embla__slide">
           <div class="container">
-            <div class="flex justify-center items-center px-5 bg-green-100 py-2 border rounded-lg">
-              <div class="w-[50%] flex flex-col gap-5 items-start ">
-                <h3 class="font-medium text-[#3D3D3D] max-sm:font-[10px]">WELCOME TO GREENSHOP</h3>
-                <h1 class="md:text-[70px] font-black max-sm:leading-[30px] md: leading-[70px]">
+            <div
+              class="flex justify-center items-center px-5 bg-green-100 py-2 border rounded-lg"
+            >
+              <div class="w-[50%] flex flex-col gap-5 items-start">
+                <h3 class="font-medium text-[#3D3D3D] max-sm:font-[10px]">
+                  WELCOME TO GREENSHOP
+                </h3>
+                <h1
+                  class="md:text-[70px] font-black max-sm:leading-[30px] md: leading-[70px]"
+                >
                   LET'S MAKE A BETTER <span class="text-green-600">PLANT</span>
                 </h1>
-                <p class="md:w-[587px] text-[#727272] font-normal max-sm:hidden">
+                <p
+                  class="md:w-[587px] text-[#727272] font-normal max-sm:hidden"
+                >
                   We are an online plant shop offering a wide range of cheap and
                   trendy plants. Use our plants to create an unique Urban
                   Jungle. Order your favorite plants!
@@ -32,16 +40,27 @@
         </div>
         <div class="embla__slide">
           <div class="container">
-            <div class="flex justify-between items-center px-5">
+            <div
+              class="flex justify-center items-center px-5 bg-green-100 py-2 border rounded-lg"
+            >
               <div class="w-[50%] flex flex-col gap-5 items-start">
-                <h3 class="font-medium text-[#3D3D3D]">WELCOME TO GREENSHOP</h3>
-                <h1 class="text-[70px] font-black leading-[70px]">
+                <h3 class="font-medium text-[#3D3D3D] max-sm:font-[10px]">
+                  WELCOME TO GREENSHOP
+                </h3>
+                <h1
+                  class="md:text-[70px] font-black max-sm:leading-[30px] md: leading-[70px]"
+                >
                   LET'S MAKE A BETTER <span class="text-green-600">PLANT</span>
                 </h1>
-                <p class="w-[587px] text-[#727272] font-normal">
+                <p
+                  class="md:w-[587px] text-[#727272] font-normal max-sm:hidden"
+                >
                   We are an online plant shop offering a wide range of cheap and
                   trendy plants. Use our plants to create an unique Urban
                   Jungle. Order your favorite plants!
+                </p>
+                <p class="md:hidden text-[#727272] font-normal">
+                  We are an online plant shop offering a wide range
                 </p>
                 <button
                   class="py-3 px-8 mt-10 bg-green-600 border rounded-md text-white"
@@ -57,16 +76,27 @@
         </div>
         <div class="embla__slide">
           <div class="container">
-            <div class="flex justify-between items-center px-5">
+            <div
+              class="flex justify-center items-center px-5 bg-green-100 py-2 border rounded-lg"
+            >
               <div class="w-[50%] flex flex-col gap-5 items-start">
-                <h3 class="font-medium text-[#3D3D3D]">WELCOME TO GREENSHOP</h3>
-                <h1 class="text-[70px] font-black leading-[70px]">
+                <h3 class="font-medium text-[#3D3D3D] max-sm:font-[10px]">
+                  WELCOME TO GREENSHOP
+                </h3>
+                <h1
+                  class="md:text-[70px] font-black max-sm:leading-[30px] md: leading-[70px]"
+                >
                   LET'S MAKE A BETTER <span class="text-green-600">PLANT</span>
                 </h1>
-                <p class="w-[587px] text-[#727272] font-normal">
+                <p
+                  class="md:w-[587px] text-[#727272] font-normal max-sm:hidden"
+                >
                   We are an online plant shop offering a wide range of cheap and
                   trendy plants. Use our plants to create an unique Urban
                   Jungle. Order your favorite plants!
+                </p>
+                <p class="md:hidden text-[#727272] font-normal">
+                  We are an online plant shop offering a wide range
                 </p>
                 <button
                   class="py-3 px-8 mt-10 bg-green-600 border rounded-md text-white"
@@ -114,4 +144,3 @@ export default {
   min-width: 0;
 }
 </style>
-
